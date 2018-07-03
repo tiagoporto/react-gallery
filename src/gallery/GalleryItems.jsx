453f3d929e2bd73a27b3396458ae330b760fdd15
Tiago Porto <tiagoporto@ymail.com>
2018-07-02 23:17:29 -0300

    Add carousel

--- src/gallery/GalleryItems.jsx
+++ src/gallery/GalleryItems.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import '../styles/css/GalleryItems.css'
 import Modal from 'react-responsive-modal'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.css'
 
 export class GalleryItems extends Component {
   state = {
-    opened: false
+    opened: false,
+    selected: null
   }
 
-  onOpenModal = () => {
-    this.setState({ opened: true })
+  onOpenModal = position => {
+    this.setState({
+      opened: true,
+      selected: position
+    })
   }
 
   onCloseModal = () => {
@@ -18,26 +24,53 @@ export class GalleryItems extends Component {
   render() {
     let content = []
 
+    let carousel = []
+
     if(this.props.images) {
       const items = []
+      const carouselItems = []
+
       this.props.images.forEach((item, position) => {
         if (position < 50) {
           items.push(
-            <button className="items" key={item.id} onClick={this.onOpenModal}>
+            <button className="items" key={item.id} onClick={() => this.onOpenModal(position)}>
               <img src={item.thumbnailUrl} alt={item.title} />
             </button>
+          )
+
+          carouselItems.push(
+            <div>
+                <img src={item.url} alt={item.title}/>
+                <p className="legend">{item.title}</p>
+            </div>
           )
         }
       })
 
       content = items
+
+      carousel = carouselItems
     }
 
     return (
       <div className="main">
         {content}
-        <Modal open={this.state.opened} onClose={this.onCloseModal} center>
-          <h2>Simple centered modal</h2>
+        <Modal
+          open={this.state.opened}
+          onClose={this.onCloseModal}
+          center
+          >
+          <Carousel
+            showArrows={true}
+            showThumbs={false}
+            dynamicHeight
+            selectedItem={this.state.selected}
+            showIndicators={false}
+            emulateTouch
+            width="100%"
+            >
+              {carousel}
+          </Carousel>
         </Modal>
       </div>
     )

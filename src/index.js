735c90bb87cd1777c3fd2db5e96a81d34ba5990b
Tiago Porto <tiago@tiagoporto.com>
2025-02-10 11:20:07 -0300

    Update react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
+
 registerServiceWorker();
